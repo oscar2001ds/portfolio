650dc51f61d823d6b1d1a1bc7eff1fcc4d90c5da
oscar <oscar2001ds@gmail.com>
2025-10-05 18:08:05 -0500

    fix(env): update STRAPI_API_URL to use VITE_STRAPI_API_URL for compatibility with Vite and Netlify

--- src/services/getHomePageData.service.js
+++ src/services/getHomePageData.service.js
@@ -1,7 +1,7 @@
 import { HOME_PAGE_QUERY } from "../queries/HomePageQuery.graphql";
 import serializeResponse from "../utils/serializeResponse";
 
-const STRAPI_API_URL = import.meta.env.STRAPI_API_URL || 'http://localhost:1337';
+const STRAPI_API_URL = import.meta.env.VITE_STRAPI_API_URL || 'http://localhost:1337';
 
 export const getHomePageData = async () => {
   try {
